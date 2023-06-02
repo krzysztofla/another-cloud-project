57a5274fe16b9c03c16556a083e7e7af2bf8e22d
Krzysztof <krzysztof.lach@icloud.com>
2023-06-02 16:14:42 +0200

    defining path with static array of objects

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/demo-go-api/models"
 	"github.com/demo-go-api/services"
 	"github.com/gin-gonic/gin"
 )
@@ -21,6 +22,19 @@ func main() {
 			})
 		}
 	})
+
+	r.GET("/items-static", func(c *gin.Context) {
+		items := models.Items{
+			models.Item{ID: 1, Name: "Item 1", Description: "Description 1"},
+			models.Item{ID: 2, Name: "Item 2", Description: "Description 2"},
+			models.Item{ID: 3, Name: "Item 3", Description: "Description 3"},
+			models.Item{ID: 4, Name: "Item 4", Description: "Description 4"},
+		}
+		c.JSON(200, gin.H{
+			"items": items,
+		})
+	})
+
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ok",
